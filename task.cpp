ba9226381fb199a6bfceff65f4e6172281ac56d1
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-04 13:38:36 +0300

    task is done

--- task.cpp
+++ task.cpp
@@ -1,6 +1,33 @@
 // Вариант 7
 
+#include <iostream>
+#include <cmath>
+
+
+int f(int x)
+{
+    int result = ((x - x % 10) / 10) % 10;
+
+    return result;
+}
+
+
 int main()
 {
+    int a, b, c;
+
+    std::cout << "Input a: ";
+    std::cin >> a;
+
+    std::cout << "Input b: ";
+    std::cin >> b;
+
+    std::cout << "Input c: "; 
+    std::cin >> c;
+
+    int z = f(a) + f(b) - f(c);
+
+    std::cout << "z = " << z << std::endl;
+
     return 0;
 }
