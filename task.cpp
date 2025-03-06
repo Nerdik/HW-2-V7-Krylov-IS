f0036b7a2f7fc809825e7aae48a081b4c09800f1
Nerdik <87544512+Nerdik@users.noreply.github.com>
2025-03-06 10:42:11 +0300

    name changed and function optimized

--- task.cpp
+++ task.cpp
@@ -4,9 +4,9 @@
 #include <cmath>
 
 
-int f(int x)
+int getSecondNum(int x)
 {
-    int result = ((x - x % 10) / 10) % 10;
+    int result = (x / 10) % 10;
 
     return result;
 }
@@ -14,6 +14,7 @@ int f(int x)
 
 int main()
 {
+
     int a;
     std::cout << "Input a: ";
     std::cin >> a;
@@ -26,7 +27,7 @@ int main()
     std::cout << "Input c: "; 
     std::cin >> c;
 
-    int z = f(a) + f(b) - f(c);
+    int z = getSecondNum(a) + getSecondNum(b) - getSecondNum(c);
 
     std::cout << "z = " << z << std::endl;
 
